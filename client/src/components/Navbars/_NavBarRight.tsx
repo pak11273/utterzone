bfeed6da4ccd1a53f1902d2873785cac52099e54
pak11273 <pak11273@gmail.com>
2020-08-23 13:50:42 -0500

    change sign to log

--- client/src/components/Navbars/_NavBarRight.tsx
+++ client/src/components/Navbars/_NavBarRight.tsx
@@ -8,7 +8,7 @@ export const RightMenu: React.FC<NavBarProps> = () => {
   return (
     <Menu mode="horizontal">
       <Menu.Item key="mail">
-        <Link to="/login">Sign In</Link>
+        <Link to="/login">Log In</Link>
       </Menu.Item>
       <Menu.Item key="app">
         <Link to="/register">Sign Up</Link>
