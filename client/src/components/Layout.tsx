186e25c7ac96b8ebfaefa186e81b57f6b58bfdd5
pak11273 <pak11273@gmail.com>
2020-08-23 12:01:26 -0500

    adds minheight 100vh to layout

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -10,7 +10,9 @@ interface LayoutProps {
 
 export const Layout: React.FC<LayoutProps> = ({ children, variant }) => {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div
+      style={{ display: "flex", minHeight: "100vh", justifyContent: "center" }}
+    >
       <Col xs={24} sm={22} md={16}>
         {children}
       </Col>
