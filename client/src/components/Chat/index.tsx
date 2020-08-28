4e8617f95cd04d7f96e5ad742d892ad00543a875
pak11273 <pak11273@gmail.com>
2020-08-28 12:34:06 -0500

    adds scrolltobot feature on chat panel

--- client/src/components/Chat/index.tsx
+++ client/src/components/Chat/index.tsx
@@ -1,7 +1,8 @@
+import React, { useEffect } from "react"
+
 import { Gravatar } from "../../components"
 import { Message } from "./Message"
 import { MoreOutlined } from "@ant-design/icons"
-import React from "react"
 
 interface indexProps {}
 
@@ -20,16 +21,44 @@ const Message2 = () => {
 }
 
 export const Chat: React.FC<indexProps> = () => {
+  useEffect(() => {
+    // ref: https://stackoverflow.com/questions/18614301/keep-overflow-div-scrolled-to-bottom-unless-user-scrolls-up
+    var out = document.getElementById("out")
+    console.log("out: ", out)
+    var c = 0
+    var add = setInterval(function () {
+      console.log("scrollheight: ", out!.scrollHeight)
+      console.log("clientheight: ", out!.clientHeight)
+      console.log("scrolltop: ", out!.scrollTop)
+      if (out) {
+        // allow 1px inaccuracy by adding 1
+        var isScrolledToBottom =
+          out!.scrollHeight - out!.clientHeight <= out!.scrollTop + 1
+        console.log(out!.scrollHeight - out!.clientHeight, out!.scrollTop + 1)
+        var newElement = document.createElement("div")
+        newElement.innerHTML = (c++).toString()
+        out!.appendChild(newElement)
+        // scroll to bottom if isScrolledToBotto
+        if (isScrolledToBottom)
+          out!.scrollTop = out!.scrollHeight - out!.clientHeight
+      }
+    }, 1000)
+    return () => {
+      clearInterval(add)
+    }
+  }, [])
+
   return (
     <div className="chat_container">
       <div className="chat_header"> Header </div>
-      <div className="chat_message--container">
+      <div className="chat_message--container" id="out">
         <Message2 />
-        {[1, 1, 1, 1, 1, 1, 1].map((x, i) => (
+        {[1, 1, 1].map((x, i) => (
           <div key={i}>
             <Message />
           </div>
         ))}
+        <Message2 />
         <Message2 />
       </div>
     </div>
